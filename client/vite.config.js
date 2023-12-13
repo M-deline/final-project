da498d6d497ce003b1ffd3c57927a22bead70be7
Madeline K <madkovanda@gmail.com>
2023-12-13 14:24:21 -0700

    update html and vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
     port: 3000,
     open: true,
     proxy: {
