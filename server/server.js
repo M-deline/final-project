8ad2e42839387e9eda160b7ef08a3a553a9e410a
Madeline K <madkovanda@gmail.com>
2023-12-13 13:20:34 -0700

    update server

--- server/server.js
+++ server/server.js
@@ -25,12 +25,12 @@ app.use(express.static(path.join(__dirname, '../client'), {
   }
 }));
 
-// if we're in production, serve client/build as static assets
+// if we're in production, serve client/dist as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
   });
 }
 
