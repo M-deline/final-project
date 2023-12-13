735b384e6cd8196eaa959fe6e032c2482b8593f0
Madeline K <madkovanda@gmail.com>
2023-12-13 14:44:39 -0700

    update eslint think the error might be bc of this file

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,17 +1,27 @@
 module.exports = {
-    "env": {
-      "node": true,
-       browser: true, es2020: true },
-  extends: [
+  "env": {
+    "node": true,
+     "browser": true, 
+     "es2020": true 
+  },
+  "parser": "@babel/eslint-parser",
+  "extends": [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
+  "parserOptions": { 
+    "ecmaVersion": 'latest', 
+    "sourceType": 'module' 
+  },
+  "settings": { 
+    "react": { 
+      "version": '18.2' 
+    } 
+  },
+  "plugins": ['react-refresh'],
+  "rules": {
     'react-refresh/only-export-components': 'warn',
   },
 }
