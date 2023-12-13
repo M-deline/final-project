382f15ee69f45e638d9f7484f3d6a6071d9fd583
Madeline K <madkovanda@gmail.com>
2023-12-13 09:27:42 -0700

    heroku issue econ refused, edit config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techmatchup');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techmatchup', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Database connected successfully'))
+.catch(err => console.error('Database connection error: ', err));
 
 module.exports = mongoose.connection;
